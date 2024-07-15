ff58c8beab44b225e397fb96fbf6817cc27e97db
Nitin Pal <141559894+nitinpal90@users.noreply.github.com>
2024-07-15 11:59:28 +0530

    Create 1581. Customer Who Visited but Did Not Make Any Transactions.sql

---
+++ 1581. Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,29 @@
+Table: Visits
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| visit_id    | int     |
+| customer_id | int     |
++-------------+---------+
+visit_id is the column with unique values for this table.
+This table contains information about the customers who visited the mall.
+ 
+Table: Transactions
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| transaction_id | int     |
+| visit_id       | int     |
+| amount         | int     |
++----------------+---------+
+transaction_id is column with unique values for this table.
+This table contains information about the transactions made during the visit_id.
+
+Write a solution to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+Return the result table sorted in any order.
+
+-- Solution --
+
+select customer_id ,count(customer_id) as count_no_trans from Visits WHERE  NOT EXISTS (SELECT visit_id
+                   FROM   Transactions t
+                   WHERE  t.visit_id = Visits.visit_id) group by customer_id
